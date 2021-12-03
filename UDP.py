3ee31b0ef3a615049f14626bc5b423538cbefc23
paulamendez <paulamendez1024@gmail.com>
2021-12-03 18:56:45 +0100

    UDP

---
+++ UDP.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  3 18:53:12 2021
+
+@author: paulamendez
+"""
+
+#UDP
+
+#this function will only be used if in the IP level UDP = true
+
+octects = ['0F','04','78','56','07','03','20','44','56','78','56','38','92','00','92','20','60','56','78','56','00','03','00','56','04','56','38','92','08','08','D4']
+
+def UDP(octects):
+    
+    #Source Port
+    
