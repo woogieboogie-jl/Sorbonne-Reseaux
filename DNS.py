63a2126755b4fd4dd937c7faf4cefb0ddc9e2d61
paulamendez <paulamendez1024@gmail.com>
2021-12-04 21:48:39 +0100

    hello

--- DNS.py
+++ DNS.py
@@ -17,19 +17,40 @@ def DNS(octects):
     I = hex(int(octects[0]+octects[1],16))
     
     #Control
-    C = octects[2]+octects[3]
+    C = bin(int(octects[2]+octects[3],16))[2:]
+    QR = C[0]
+    QR_dic = {0: 'Request', 1: 'Response'}
+    OpCode = int(C[1:5],2)
+    OpCode_dic = {0: 'Query', 1: 'Inverse Query', 2:'Status', 3:'Unassigned', 4:'Notify', 5:'Update', 6:'DNS Stateful Operations'}
+    AA = C[5]           #Authoritative Answer
+    AA_dic = {1: 'Authoritative', 0: 'Cache'}
+    TC = C[6]           #Truncated
+    if TC == 1:
+        TC = 'Response too large for UDP'
+    RD = C[7]           #Recursion Desired
+    RD_dic = {0:'Iterative', 1:'Recursif'}
+    RA = C[8]           #Recursion Available
+    RA_dic = {0: 'Not recursive', 1:'Recursive'}
+    Z = C[9]            #Zero reserved for extensions
+    AD = [10]           #Authenticated Data
+    CD = [11]           #Checking Disabled
+    Rcode = [12]
+    Rcode_dic = {0:'NOERROR', 1:'FORMERR', 2:'SERVFAIL', 3:'NXDOMAIN', 4:'NOTIMP', 5:'REFUSED', 6:'YXDOMAIN', 7:'XRRSET', 8:'NOTAUTH', 9:'NOTZONE' }
     
     #Question count
-    QC = octects[4] + octects[5]
+    QC = int(octects[4] + octects[5],16)
     
     #Answer count
-    AC = octects[6]+octects[7]
+    AC = int(octects[6]+octects[7],16)
     
     #Authority count
-    AUC = octects[8]+octects[9]
+    AUC = int(octects[8]+octects[9],16)
     
     #Additional count
-    ADC = octects[10]+octects[11]
+    ADC = int(octects[10]+octects[11],16)
+    
+    #DNS contains Resouce Records: RR
+    #(NAME, VALUE, TYPE, TTL)
     
     
     #Questions
@@ -37,48 +58,62 @@ def DNS(octects):
     
     #Answers
     
+    #Authority 
+    
+    
+    
+    #Additional
+    
+    
+    #Print everything
+    
+    
+    
+    
+    
+    
 DNS(octects)
 
 
-def DHCP(octects):
+# def DHCP(octects):
     
-    op = int(octects[0],16)
-    if op == 1:
-        op = 'DHCP Request Client'
-    elif op == 2:
-        op = 'DHCP Reply Serveur'
-    else:
-        op = 'Unknown type of message'
+#     op = int(octects[0],16)
+#     if op == 1:
+#         op = 'DHCP Request Client'
+#     elif op == 2:
+#         op = 'DHCP Reply Serveur'
+#     else:
+#         op = 'Unknown type of message'
     
-    htype = octects[1]
+#     htype = octects[1]
     
-    hlen = octects[2]
+#     hlen = octects[2]
     
-    hops = octects[3]
+#     hops = octects[3]
     
-    xid = octects[4]+octects[5]+octects[6]+octects[7]
+#     xid = octects[4]+octects[5]+octects[6]+octects[7]
     
-    secs = octects[8]+octects[9]
+#     secs = octects[8]+octects[9]
     
-    flags = octects[10]+octects[11]
+#     flags = octects[10]+octects[11]
     
-    ciadrr = octects[12:15]
+#     ciadrr = octects[12:15]
              
-    yiadrr = octects[16:19]
+#     yiadrr = octects[16:19]
     
-    siadrr = octects[20:23]
+#     siadrr = octects[20:23]
     
-    giaddr = octects[24:27]
+#     giaddr = octects[24:27]
     
-    chaddr = octects[28:44]
+#     chaddr = octects[28:44]
     
-    #je ne suis pas sure avec ce que je dois faire avec les champs de sname et file parceque qu'ils sont optionnels
+#     #je ne suis pas sure avec ce que je dois faire avec les champs de sname et file parceque qu'ils sont optionnels
     
-    Options = octects[45:]
+#     Options = octects[45:]
     
-    Opt53 = {1: "DHCP Discover", 2:"DHCP Offer", 3:"DHCP Request", 4:"DHCP Decline", 5:"DHCP Pack", 6:"DHCP Nak", 7:"DHCP Release", 8:"DHCP Inform"}
+#     Opt53 = {1: "DHCP Discover", 2:"DHCP Offer", 3:"DHCP Request", 4:"DHCP Decline", 5:"DHCP Pack", 6:"DHCP Nak", 7:"DHCP Release", 8:"DHCP Inform"}
     
-    #on doit toujours finir la zone d’options par une option 255
+#     #on doit toujours finir la zone d’options par une option 255
     
     
     
